aa283aaf84034d9112c3ea73b930d2fe5abadc52
Sulemam-Ba <patricio_gato@hotmail.com>
2021-09-27 20:50:10 -0300

    Criação da aplicação
    Criação

---
+++ GTASOLUTION CADASTRO (AGENDA)/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace GTASOLUTION_CADASTRO__AGENDA_
+{
+    partial class AGENDA
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LIMPAR = new System.Windows.Forms.Button();
+            this.BUSCARFOTO = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CONSULTAR = new System.Windows.Forms.Button();
+            this.EXIBIR = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EXCLUIR = new System.Windows.Forms.Button();
+            this.EDITAR = new System.Windows.Forms.Button();
+            this.ADICIONAR = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(55, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 13);
+            this.label8.TabIndex = 49;
+            this.label8.Text = "RG";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "CPF";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "ENDEREÇO";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(86, 214);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(276, 20);
+            this.textBox6.TabIndex = 46;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(86, 188);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(276, 20);
+            this.textBox5.TabIndex = 45;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(86, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(276, 20);
+            this.textBox4.TabIndex = 44;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 20);
+            this.textBox1.TabIndex = 43;
+            // 
+            // LIMPAR
+            // 
+            this.LIMPAR.Location = new System.Drawing.Point(380, 216);
+            this.LIMPAR.Name = "LIMPAR";
+            this.LIMPAR.Size = new System.Drawing.Size(157, 19);
+            this.LIMPAR.TabIndex = 42;
+            this.LIMPAR.Text = "LIMPAR";
+            this.LIMPAR.UseVisualStyleBackColor = true;
+            this.LIMPAR.Click += new System.EventHandler(this.LIMPAR_Click);
+            // 
+            // BUSCARFOTO
+            // 
+            this.BUSCARFOTO.Location = new System.Drawing.Point(380, 189);
+            this.BUSCARFOTO.Name = "BUSCARFOTO";
+            this.BUSCARFOTO.Size = new System.Drawing.Size(157, 19);
+            this.BUSCARFOTO.TabIndex = 41;
+            this.BUSCARFOTO.Text = "BUSCAR";
+            this.BUSCARFOTO.UseVisualStyleBackColor = true;
+            this.BUSCARFOTO.Click += new System.EventHandler(this.BUSCARFOTO_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(433, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "FOTO";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pictureBox2.Location = new System.Drawing.Point(380, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(157, 99);
+            this.pictureBox2.TabIndex = 39;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "CADASTRO DOS CLIENTES";
+            // 
+            // CONSULTAR
+            // 
+            this.CONSULTAR.Location = new System.Drawing.Point(267, 240);
+            this.CONSULTAR.Name = "CONSULTAR";
+            this.CONSULTAR.Size = new System.Drawing.Size(85, 19);
+            this.CONSULTAR.TabIndex = 37;
+            this.CONSULTAR.Text = "CONSULTAR";
+            this.CONSULTAR.UseVisualStyleBackColor = true;
+            this.CONSULTAR.Click += new System.EventHandler(this.CONSULTAR_Click);
+            // 
+            // EXIBIR
+            // 
+            this.EXIBIR.Location = new System.Drawing.Point(176, 239);
+            this.EXIBIR.Name = "EXIBIR";
+            this.EXIBIR.Size = new System.Drawing.Size(85, 19);
+            this.EXIBIR.TabIndex = 36;
+            this.EXIBIR.Text = "EXIBIR";
+            this.EXIBIR.UseVisualStyleBackColor = true;
+            this.EXIBIR.Click += new System.EventHandler(this.EXIBIR_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.PaleTurquoise;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 264);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(521, 178);
+            this.dataGridView1.TabIndex = 35;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(276, 20);
+            this.textBox3.TabIndex = 34;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 20);
+            this.textBox2.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "TELEFONE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "NOME";
+            // 
+            // EXCLUIR
+            // 
+            this.EXCLUIR.Location = new System.Drawing.Point(452, 240);
+            this.EXCLUIR.Margin = new System.Windows.Forms.Padding(2);
+            this.EXCLUIR.Name = "EXCLUIR";
+            this.EXCLUIR.Size = new System.Drawing.Size(85, 19);
+            this.EXCLUIR.TabIndex = 29;
+            this.EXCLUIR.Text = "EXCLUIR";
+            this.EXCLUIR.UseVisualStyleBackColor = true;
+            this.EXCLUIR.Click += new System.EventHandler(this.EXCLUIR_Click);
+            // 
+            // EDITAR
+            // 
+            this.EDITAR.Location = new System.Drawing.Point(363, 241);
+            this.EDITAR.Margin = new System.Windows.Forms.Padding(2);
+            this.EDITAR.Name = "EDITAR";
+            this.EDITAR.Size = new System.Drawing.Size(85, 19);
+            this.EDITAR.TabIndex = 28;
+            this.EDITAR.Text = "EDITAR";
+            this.EDITAR.UseVisualStyleBackColor = true;
+            this.EDITAR.Click += new System.EventHandler(this.EDITAR_Click);
+            // 
+            // ADICIONAR
+            // 
+            this.ADICIONAR.Location = new System.Drawing.Point(86, 239);
+            this.ADICIONAR.Margin = new System.Windows.Forms.Padding(2);
+            this.ADICIONAR.Name = "ADICIONAR";
+            this.ADICIONAR.Size = new System.Drawing.Size(85, 19);
+            this.ADICIONAR.TabIndex = 27;
+            this.ADICIONAR.Text = "ADICIONAR";
+            this.ADICIONAR.UseVisualStyleBackColor = true;
+            this.ADICIONAR.Click += new System.EventHandler(this.ADICIONAR_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GTASOLUTION_CADASTRO__AGENDA_.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 50);
+            this.pictureBox1.TabIndex = 50;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AGENDA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(551, 453);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LIMPAR);
+            this.Controls.Add(this.BUSCARFOTO);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CONSULTAR);
+            this.Controls.Add(this.EXIBIR);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EXCLUIR);
+            this.Controls.Add(this.EDITAR);
+            this.Controls.Add(this.ADICIONAR);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "AGENDA";
+            this.Text = "GTASOLUTION CADASTRO";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button LIMPAR;
+        private System.Windows.Forms.Button BUSCARFOTO;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button CONSULTAR;
+        private System.Windows.Forms.Button EXIBIR;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EXCLUIR;
+        private System.Windows.Forms.Button EDITAR;
+        private System.Windows.Forms.Button ADICIONAR;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GTASOLUTION CADASTRO (AGENDA)/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//FORAM USADO ESTA 2 BIBLIOTECA PARA ESTA APLIACAÇÃO
+using System.Data.SqlClient;
+using System.IO;
+
+namespace GTASOLUTION_CADASTRO__AGENDA_
+{
+    public partial class AGENDA : Form
+    {
+        //DECLARAÇÃO DAS VARIÁVEIS
+        SqlConnection conexão;
+        SqlCommand comando;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        Bitmap bmp;
+
+        string strSQL;
+
+        public AGENDA()
+        {
+            InitializeComponent();
+        }
+
+        //BOTÃO ADICIONAR
+        private void ADICIONAR_Click(object sender, EventArgs e)
+        {
+            try
+            {// STRING DE INTEGRAÇÃO COM BANCO DE DADOS
+                conexão = new SqlConnection("Data Source=DESKTOP-51ADQ9D\\SQLEXPRESS;Initial Catalog=GTASOLUTION;Integrated Security=True");
+
+                //STRING DE INSERÇÃO A TABELA 
+                strSQL = "INSERT INTO gtatabela ( nome, telefone, endereço, cpf, rg, foto) VALUES ( @NOME, @TELEFONE, @ENDEREÇO, @CPF, @RG, @FOTO )";
+
+                comando = new SqlCommand(strSQL, conexão);
+
+                //CODIGO DE CONVERSÃO DE IMAGEM PARA O BANCO DE DADOS
+                Image img = pictureBox2.Image;
+                byte[] arr;
+                ImageConverter converter = new ImageConverter();
+                arr = (byte[])converter.ConvertTo(img, typeof(byte[]));
+
+                //CODIGO PARA ADICIONAR OS VALORES DOS TEXTBOX NA TABELA 
+                comando.Parameters.AddWithValue("@NOME", textBox2.Text);
+                comando.Parameters.AddWithValue("@TELEFONE", textBox3.Text);
+                comando.Parameters.AddWithValue("@ENDEREÇO", textBox4.Text);
+                comando.Parameters.AddWithValue("@CPF", textBox5.Text);
+                comando.Parameters.AddWithValue("@RG", textBox6.Text);
+                comando.Parameters.AddWithValue("@FOTO", arr);
+
+                //LIMPAR OS CAMPOS
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+
+                //LIMPA O CAMPO DAS FOTOS 
+                pictureBox2.Image = null;
+
+                //ABRE CONEXÃO DO BANCO E RETORNA MENSAGEM DE CASTRAMENTO COM SUCESSO
+                conexão.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Usuário cadastrado com seucesso");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //FECHANDO CONEXÃO COM BANCO DE DADOS
+                conexão.Close();
+                conexão = null;
+                comando = null;
+            }
+        }
+
+        //BOTÃO EXIBIR
+        private void EXIBIR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // INTEGRAÇÃO COM BANCO DE DADOS
+                conexão = new SqlConnection("Data Source=DESKTOP-51ADQ9D\\SQLEXPRESS;Initial Catalog=GTASOLUTION;Integrated Security=True");
+
+                //EXIBIR BANCO DE DADOS 
+                strSQL = "SELECT * FROM  gtatabela";
+
+                DataSet ds = new DataSet();
+
+                da = new SqlDataAdapter(strSQL, conexão);
+
+                conexão.Open();
+
+                da.Fill(ds);
+
+                dataGridView1.DataSource = ds.Tables[0];
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexão.Close();
+                conexão = null;
+                comando = null;
+            }
+        }
+
+        private void CONSULTAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // INTEGRAÇÃO COM BANCO DE DADOS
+                conexão = new SqlConnection("Data Source=DESKTOP-51ADQ9D\\SQLEXPRESS;Initial Catalog=GTASOLUTION;Integrated Security=True");
+
+                //SELECIONAR TABELA POR ID
+                strSQL = "SELECT * FROM gtatabela WHERE ID = @ID";
+
+                comando = new SqlCommand(strSQL, conexão);
+
+                comando.Parameters.AddWithValue("@ID", textBox1.Text);
+
+
+                conexão.Open();
+                dr = comando.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    textBox2.Text = (string)dr["nome"];
+                    textBox3.Text = (string)dr["telefone"];
+                    textBox4.Text = (string)dr["endereço"];
+                    textBox5.Text = (string)dr["cpf"];
+                    textBox6.Text = (string)dr["rg"];
+
+                    textBox2.Text = Convert.ToString(dr["nome"]);
+                    textBox3.Text = Convert.ToString(dr["telefone"]);
+                    textBox4.Text = Convert.ToString(dr["endereço"]);
+                    textBox5.Text = Convert.ToString(dr["cpf"]);
+                    textBox6.Text = Convert.ToString(dr["rg"]);
+                    MemoryStream ms = new MemoryStream((byte[])dr["foto"]);
+                    pictureBox2.Image = Image.FromStream(ms);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexão.Close();
+                conexão = null;
+                comando = null;
+            }
+        }
+
+        private void EDITAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // INTEGRAÇÃO COM BANCO DE DADOS
+                conexão = new SqlConnection("Data Source=DESKTOP-51ADQ9D\\SQLEXPRESS;Initial Catalog=GTASOLUTION;Integrated Security=True");
+                
+                //STRING DE ALTERAÇÃO DO TABELA 
+                strSQL = "UPDATE gtatabela SET NOME = @NOME, TELEFONE = @TELEFONE, ENDEREÇO = @ENDEREÇO, CPF = @CPF, RG = @RG WHERE ID = @ID";
+
+
+                comando = new SqlCommand(strSQL, conexão);
+
+                //CODIGO PARA ADICIONAR OS VALORES DOS TEXTBOX NA TABELA 
+                comando.Parameters.AddWithValue("@ID", textBox1.Text);
+                comando.Parameters.AddWithValue("@NOME", textBox2.Text);
+                comando.Parameters.AddWithValue("@TELEFONE", textBox3.Text);
+                comando.Parameters.AddWithValue("@ENDEREÇO", textBox4.Text);
+                comando.Parameters.AddWithValue("@CPF", textBox5.Text);
+                comando.Parameters.AddWithValue("@RG", textBox6.Text);
+
+                //LIMPAR TODOS CAMPOS
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+
+                pictureBox2.Image = null;
+
+                conexão.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Usuário editado com seucesso");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexão.Close();
+                conexão = null;
+                comando = null;
+            }
+        }
+
+        private void EXCLUIR_Click(object sender, EventArgs e)
+        {
+            try
+            {// STRING DE INTEGRAÇÃO COM BANCO DE DADOS
+                conexão = new SqlConnection("Data Source=DESKTOP-51ADQ9D\\SQLEXPRESS;Initial Catalog=GTASOLUTION;Integrated Security=True");
+
+                //STRING DE EXCLUSÃO DA TABELA POR ID
+                strSQL = "DELETE gtatabela WHERE ID = @ID";
+
+                comando = new SqlCommand(strSQL, conexão);
+
+                comando.Parameters.AddWithValue("@ID", textBox1.Text);
+
+                //LIMPAR TODOS CAMPOS
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+                pictureBox2.Image = null;
+
+
+                conexão.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Usuário deletado com seucesso");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexão.Close();
+                conexão = null;
+                comando = null;
+            }
+        }
+        //LOCALIZAR IMAGEM PARA INSERIR NO BANCO DE DADOS
+        private void BUSCARFOTO_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.ImageLocation = openFileDialog1.FileName;
+                string nome = openFileDialog1.FileName;
+
+                bmp = new Bitmap(nome);
+
+                pictureBox2.Image = bmp;
+
+            }
+        }
+
+        private void LIMPAR_Click(object sender, EventArgs e)
+        {
+            //LIMPAR TODOS CAMPOS
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            pictureBox2.Image = null;
+        }
+    }
+}
